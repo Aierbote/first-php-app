d6d03d538f886233b032df92bc33789e07f5874d
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-17 23:10:54 +0100

    feat: properties for Context class

---
+++ Context.php
@@ -0,0 +1,103 @@
+<?php
+
+
+class Context
+{
+	// Properties
+	private static $cart = array();
+	private static $paid = false;
+	private static $products = NULL;
+	private static $loading = false;
+	private static $error = "";
+	private static $counterInCart = 0;
+	private static $total = 0;
+	private static $detailProduct = (object) []; // TODO : NOTE : IDK maybe it could be a "object literal" like in js
+
+	public function __construct()
+	{
+		$this->cart = array();
+		$this->paid = false;
+		$this->products = NULL;
+		$this->loading = false;
+		$this->error = "";
+		$this->counterInCart = 0;
+		$this->total = 0;
+
+		# NOTE : associative array casted into an object, closest to a object literal in js
+		// $this->detailProduct = (object) [
+		// 	"qty"-> ,
+		// 	"userId"-> ,
+		// 	"title"-> ,
+		// 	"description"-> ,
+		// 	"id"-> ,
+		// 	"price"-> ,
+		// 	"image"-> ,
+		// 	"thumbnail"-> ,
+		// ];
+
+	}
+
+	// Methods
+	public function addToCart()
+	{
+		return null;
+	}
+
+	public function removeFromCart()
+	{
+		return null;
+	}
+
+	public function removeTheseFromCart()
+	{
+		return null;
+	}
+
+	public function pay()
+	{
+		return null;
+	}
+
+	public function done()
+	{
+		return null;
+	}
+
+	public function getProductQuantity()
+	{
+		return null;
+	}
+
+	public function getProduct()
+	{
+		return null;
+	}
+
+
+	public function repr()
+	{
+		return "Context(cart=" .
+			$this->cart .
+			", paid=" .
+			$this->paid .
+			", products=" .
+			$this->products .
+			", loading=" .
+			$this->loading .
+			", error=" .
+			$this->error .
+			", counterInCart=" .
+			$this->counterInCart .
+			", total=" .
+			$this->total .
+			", detailProduct=" .
+			$this->detailProduct .
+			")";
+	}
+};
+
+$myContext = new Context();
+
+
+echo " DEBUG : Context \n";
+echo $myContext->repr();
